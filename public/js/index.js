f272126ab55f1af5913f1024b34a8eb3dec2a242
AndreasSjo <andreas.sjoden@hotmail.com>
2022-02-14 13:14:32 +0100

    add button

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,7 @@
 var aDiv = document.getElementById('app');
+var aButton = document.createElement('button');
+    aButton.innerHTML = 'add row';
+    aButton.onclick = addOnClick();
 
 var aTable = document.createElement('table');
     aTable.style.width = '100%';
@@ -12,3 +15,11 @@ var aTable = document.createElement('table');
         }
 
 aDiv.appendChild(aTable);
+aDiv.appendChild(aButton);
+
+function addOnClick(){
+    var aRow = document.createElement('tr');
+            var aText = document.createTextNode("New row");
+                aRow.appendChild(aText);
+            aTable.appendChild(aRow);
+}
