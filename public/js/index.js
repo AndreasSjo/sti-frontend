30fe32ade2e77ba3a9b3b2680acbd1987272e88c
AndreasSjo <andreas.sjoden@hotmail.com>
2022-02-14 10:33:29 +0100

    add table and rows to index.js

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+var aDiv = document.getElementById('app');
+    aDiv.createTextNode("test");
+var aTable = document.createElement('table');
+    aTable.style.width = '100%';
+    aTable.setAttribute('border', '1');
+
+        for (var i = 0; i < 3; i++){
+            var aRow = document.createElement('tr');
+            var aText = document.createTextNode("textrow number: " + i);
+                aRow.appendChild(aText);
+            aTable.appendChild(aRow);
+        }
+aDiv.appendChild(aTable);
