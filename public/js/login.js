afd5e610223c6eb44e42e91638d00845a2152bc2
OskarMartens <martens.oskar@gmail.com>
2022-02-22 15:57:29 +0100

    add to login.js login get func

--- public/js/login.js
+++ public/js/login.js
@@ -10,3 +10,15 @@ app.listen(5000, () => {
     console.log("Server online")
 })
 
+age()
+
+function age(){
+    var xhr = new XMLHttpRequest()
+    xhr.open("GET", "http://localhost:3001/user")
+    xhr.onload = function(){
+        var data = JSON.parse(this.response)
+        createTable(data)
+    }
+    xhr.send()
+}
+
