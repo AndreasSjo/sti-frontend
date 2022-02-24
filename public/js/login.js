dbfb2ab23fc79b9bf8db4e97c4356714649c1279
OskarMartens <martens.oskar@gmail.com>
2022-02-24 22:59:58 +0100

    made changes to login

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,48 @@
-const express = require("express");
+var jwt = localStorage.getItem("jwt");
+if (jwt != null) {
+    window.location.href = './index.html'
+}
+
+function login(){
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("POST", /* Länk till backend */)
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.send(JSON.stringify({
+        "username": username,
+        "password": password
+    }));
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            const objects = JSON.parse(this.responseText);
+            console.log(objects);
+            if (objects['status'] == 'ok') {
+                localStorage.setItem("jwt", objects['accessToken']);
+                Swal.fire({                             /*Swal används för att göra pop-up vid login  */
+                    text: objects['message'],
+                    icon: 'success',
+                    confirmButtonText: 'Okej!'
+                }).then((result) => {
+                    if(result.isConfirmed) {
+                        window.location.href ='./index.html';
+                    }
+                }); 
+            } else {
+                Swal.fire({
+                    text: objects['message'],
+                    icon: 'error',
+                    confirmButtonText: 'Okej!'
+                });
+            }
+        }
+    };
+    return false;
+}
+
+
+/* const express = require("express");
 
 const app = express();
 
@@ -22,3 +66,4 @@ function age(){
     xhr.send()
 }
 
+ */
