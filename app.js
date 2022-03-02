95eec41b735db7cf14c6fd832d5560391692383e
AndreasSjo <andreas.sjoden@hotmail.com>
2022-03-02 11:28:09 +0100

    login

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 const express = require("express")
 var favicon = require('serve-favicon')
+const session = require('express-session');
+const mysql = require('mysql');
 var path = require('path')
+const connection = mysql.createConnection({
+	host     : 'localhost:3306',
+	user     : 'root',
+	password : 'root',
+	database : 'filmerDB'
+});
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+
 app.use(express.static('public'))
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + "/public/index.html")
-})
+
 
 app.get('/', function(req, res){
+
     res.sendFile(__dirname + "/public/login.html")
 })
+
+
+
 
 app.listen(PORT, function(){
     console.log(`Server started on port ${PORT}`)
--- public/js/login.js
+++ public/js/login.js
@@ -1,82 +1,14 @@
-var jwt = localStorage.getItem("jwt");
-if (jwt != null) {
-    window.location.href = './index.html'
-}
-
-
 function login(){
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
     
     const xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "http://localhost:3001/users"); /* Länk till backend */
+    xhttp.open("POST", "http://localhost:3001/auth");
     xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xhttp.send (JSON.stringify({
         "username": username,
         "password": password
     }));
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-            const objects = JSON.parse(this.responseText);
-            console.log(objects);
-            console.log(objects[4].status);
-            if (objects['status'] == 'ok') {
-                localStorage.setItem("jwt", objects['accessToken'])
-                Swal.fire({
-                    text: objects['message'],
-                    icon: 'success',
-                    confirmButtonText: 'OK'
-                }).then((result) => {
-                    if(result.isConfirmed) {
-                        window.location.href ='./index.html';
-                    }
-                }); 
-            } else {
-                Swal.fire({
-                    text: objects['message'],
-                    icon: 'error',
-                    confirmButtonText: 'Okej!'
-                });
-            }
-        }
-    };
-    return false;
 }
 
 
-/* const express = require("express");
-
-const app = express();
-
-app.get("/", (req, res) => {
-    res.send()
-})
-
-app.listen(5000, () => {
-    console.log("Server online")
-})
-
-age()
-
-function age(){
-    var xhr = new XMLHttpRequest()
-    xhr.open("GET", "http://localhost:3001/user")
-    xhr.onload = function(){
-        var data = JSON.parse(this.response)
-        createTable(data)
-    }
-    xhr.send()
-}
-
-*/
-
-/* const express = require("express")
-
-
-const app = express()
-
-app.get("/users", (req ,res)=>{
-    headers={"http_status":200, "cache-control":  "no-cache"}
-    res.sendFile(path.join(__dirname, '/users.json'));
-    
- }) */
